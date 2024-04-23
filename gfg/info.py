3ac2341059e3a56dbff2dca89c19c55b9a8f161d
amalthobyy <amalpthobias@gmail.com>
2024-04-23 17:38:30 +0530

    second commit

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,6 @@
+from decouple import config
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amal.buss999@gmail.com'
-EMAIL_HOST_PASSWORD = 'omwh fxcp tdev djte'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password of the email'
 EMAIL_PORT = 587
